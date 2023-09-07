bbee26b84550d0f7610663a886b1b12dcc629548
Than Htet Aung <thanhtetaung87@gmail.com>
2023-09-07 10:59:59 +0900

    - added build script - fixed not to separate css file in build

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
+    extract: false,
     loaderOptions: {
       sass: {
         additionalData: `
